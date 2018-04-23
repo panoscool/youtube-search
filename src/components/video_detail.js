6c43263d814b3a1e8af330db641e678c02064a85
Panos K <vladi_lk@hotmail.com>
2018-04-23 20:54:16 +0400

    initial commit

---
+++ src/components/video_detail.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../style/video_detail.css';
+
+const VideoDetail = ({video}) => {
+
+    if(!video) {
+        return <div></div>;
+    }
+
+    const videoId = video.id.videoId;
+    const url = `https:www.youtube.com/embed/${videoId}`;
+    // equal to const url = 'https:www.youtube.com/embed/' + videoId;
+    return (
+        <div className="video-detail col-md-8">
+            <div className="embed-responsive embed-responsive-16by9">
+                <iframe className="embed-responsive-item" src={url}> </iframe>
+            </div>
+            <div className="details">
+                <div>{video.snippet.title}</div>
+                <div>{video.snippet.description}</div>                
+            </div>
+        </div>
+    );
+}
+
+export default VideoDetail;
