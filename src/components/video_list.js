4801d54dade4dddc4b64eb4f71bb8d26d811f508
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2018-05-13 13:11:30 +0300

    Fixes padding

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,11 +2,11 @@ import React from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = ({ videos, app}) => (
-    <ul className="list">
-        {videos.map((video) => 
-            <VideoListItem app={app} key={video.etag} video={video} />
-        )}
-    </ul>
+  <ul className="list">
+    {videos.map((video) =>
+      <VideoListItem app={app} key={video.etag} video={video} />
+    )}
+  </ul>
 )
 
 export default VideoList;
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,21 +2,21 @@ import React from 'react';
 import '../style/video_list_item.css';
 
 const VideoListItems = ({ app, video}) => {
-    const imageUrl = video.snippet.thumbnails.default.url;
+  const imageUrl = video.snippet.thumbnails.default.url;
 
-    return(
-        <li onClick={() => app.videoSelect(video)} className="list-group-item">
-            <div className="video-list">
-                <div className="media-left">
-                    <img className="media-object" src={imageUrl} alt="" />
-                </div>
+  return(
+    <li onClick={() => app.videoSelect(video)} className="list-group-item">
+      <div className="video-list">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} alt="" />
+        </div>
 
-                <div className="media-body">
-                    <div className="media-heading">{video.snippet.title}</div>
-                </div>
-            </div>
-        </li>
-    );
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export default VideoListItems;
