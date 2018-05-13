8b240654940d3c73660b9bdde0ad93149a01b92f
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2018-05-13 13:12:51 +0300

    Fixes padding

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -2,25 +2,24 @@ import React from 'react';
 import '../style/video_detail.css';
 
 const VideoDetail = ({video}) => {
+  if(!video) {
+    return <div></div>;
+  }
 
-    if(!video) {
-        return <div></div>;
-    }
-
-    const videoId = video.id.videoId;
-    const url = `https:www.youtube.com/embed/${videoId}`;
-    // equal to const url = 'https:www.youtube.com/embed/' + videoId;
-    return (
-        <div className="video-detail">
-            <div className="embed-responsive">
-                <iframe className="embed-responsive-item" title="video" src={url} allowFullScreen> </iframe>
-            </div>
-            <div className="details">
-                <div>{video.snippet.title}</div>
-                <div>{video.snippet.description}</div>                
-            </div>
-        </div>
-    );
+  const videoId = video.id.videoId;
+  const url = `https:www.youtube.com/embed/${videoId}`;
+  // equal to const url = 'https:www.youtube.com/embed/' + videoId;
+  return (
+    <div className="video-detail">
+      <div className="embed-responsive">
+        <iframe className="embed-responsive-item" title="video" src={url} allowFullScreen> </iframe>
+      </div>
+      <div className="details">
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default VideoDetail;
